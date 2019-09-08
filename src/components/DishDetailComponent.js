c035adea3a299d3df6522877c40e3ad8b376e6e6
Nitav Shah <shahnitav@gmail.com>
2019-09-08 16:19:38 +0530

    Functional Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);    
-    }
 
-
-    renderDish(dish) {
+function RenderDish({dish}) {
             return(<Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
                 </Card>
                 );
     }
-    renderComments(comments) {
+function RenderComments({comments}) {
         var commentList = comments.map( comment => { 
             return (
             <li key={comment.id}>
@@ -36,17 +31,18 @@ class DishDetail extends Component {
     );
     }
    
-    render() {    
-        if(this.props.dish)
+    const DishDetail = (props) => {   
+                
+        if(props.dish)
         {
             return(
                     <div className="container">
                     <div className="row">
                          <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderComments comments={props.dish.comments} />
                     </div>   
                     </div>
                     </div>         
@@ -59,6 +55,5 @@ class DishDetail extends Component {
         } 
         
         }      
-    }
 
 export default DishDetail;
